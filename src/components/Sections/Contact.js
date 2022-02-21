726bc5b11b1954c2289abd412c59d50651091a7d
Kyle Hennessy <kyle-hennessy@hotmail.com>
2022-02-21 11:36:58 +0000

    Made some changes to the contact section so that it now has a contact form. This will closely resemble the final look of the landing page as I now will go back and add more styling, implement state and make the site look nicer as a whole

--- src/components/Sections/Contact.js
+++ src/components/Sections/Contact.js
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
 import { IoMdContact } from "react-icons/io";
 
 const Contact = () => {
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="text-center">
           <IoMdContact className="w-10 inline-block mb-4 cap" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4">
-            Contact Me
+            Contact Details
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
             If you are interested in contacting me about any potential roles, or
@@ -16,98 +16,79 @@ const Contact = () => {
             to contact me through any of the methods below.
           </p>
         </div>
-    </Container>
-        <Container>
+      </Container>
+      <Container>
         <Row xs={1} md={2} className="g-4">
-          {/* <Col>
-            <Card id="socials">
-              <Card.Body>
-                <a href="https://linkedin.com">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
-                    alt="Visit my Linkedin!"
-                    className="cap"
-                  />
-                </a>
-                <a href="https://linkedin.com">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                    alt="Visit my GitHub!"
-                    className="cap"
-                  />
-                </a>
-              </Card.Body>
-              <Card.Footer>
-                <p>Socials</p>
-              </Card.Footer>
-            </Card>
-          </Col> */}
           <Col>
-            <Card id="details">
-              <Card.Body>
-                <div className="lg:w-1/2 px-6">
-                  <h2 className="title-font font-semibold tracking-widest text-xs">
-                    ADDRESS
-                  </h2>
-                  <p className="mt-1">
-                    97 Warren St. <br />
-                    New York, NY 10007
-                  </p>
-                </div>
-                <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-                  <h2 className="title-font font-semibold tracking-widest text-xs">
-                    EMAIL
-                  </h2>
-                  <a className="text-indigo-400 leading-relaxed">
-                    reedbarger@email.com
-                  </a>
-                  <h2 className="title-font font-semibold tracking-widest text-xs mt-4">
-                    PHONE
-                  </h2>
-                  <p className="leading-relaxed">123-456-7890</p>
-                </div>
-              </Card.Body>
-              <Card.Footer className="text-center">My Contact Details</Card.Footer>
-            </Card>
+            {/* <Card id="details">
+              <Card.Body> */}
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold tracking-widest text-xs">
+                ADDRESS
+              </h2>
+              <p className="mt-1">
+                97 Warren St. <br />
+                New York, NY 10007
+              </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                reedbarger@email.com
+              </a>
+              <h2 className="title-font font-semibold tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">123-456-7890</p>
+              <h2 className="title-font font-semibold tracking-widest text-xs mt-4">
+                SOCIALS
+              </h2>
+              <a href="https://linkedin.com">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
+                  alt="Visit my Linkedin!"
+                  className="social"
+                />
+              </a>
+              <a href="https://linkedin.com">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  alt="Visit my GitHub!"
+                  className="social"
+                />
+              </a>
+            </div>
           </Col>
           <Col>
-            <Card className="details">
-              <Card.Body>
-                <a href="https://linkedin.com">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/174/174857.png"
-                    alt="Visit my Linkedin!"
-                    className="cap"
-                    id="linkedin"
-                  />
-                </a>
-                <a href="https://linkedin.com">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                    alt="Visit my GitHub!"
-                    className="cap"
-                  />
-                </a>
-              </Card.Body>
-              <Card.Footer className="text-center">My Socials</Card.Footer>
-            </Card>
+            <h2 className="title-font font-semibold tracking-widest text-xs">
+              Send me a message
+            </h2>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
+              suscipit officia aspernatur veritatis. Asperiores, aliquid?
+            </p>
+            <Form>
+              <Form.Group className="mb-3" controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name"/>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email"/>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" placeholder="Leave a message here" style={{height: '100px'}}/>
+              </Form.Group>
+            </Form>
           </Col>
-          {/* <Col>
-            <Card className="details">
-              <Card.Body>
-                <a href="https://linkedin.com">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                    alt="Visit my GitHub!"
-                    className="cap"
-                  />
-                </a>
-              </Card.Body>
-            </Card>
-          </Col> */}
         </Row>
-        
-        </Container>
+      </Container>
+      <form></form>
     </section>
   );
 };
