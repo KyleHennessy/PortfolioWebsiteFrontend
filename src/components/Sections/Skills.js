acc6aa59c86e7a5eb9c5cb6f952108fd8dccf690
Kyle Hennessy <kyle-hennessy@hotmail.com>
2022-04-12 23:02:59 +0100

    Skills are now received from the backend and displayed in the frontend

--- src/components/Sections/Skills.js
+++ src/components/Sections/Skills.js
@@ -1,35 +1,57 @@
+import { useEffect, useState } from "react";
+import useHttp from "../../hooks/use-http";
+
+import SkillsList from "../Skills/SkillsList";
 import { Container } from "react-bootstrap";
 import { MdComputer } from "react-icons/md";
 
-import SkillsList from "../Skills/SkillsList";
 const Skills = () => {
-  const DUMMY_SKILLS = [
-    {
-      id: "s1",
-      title: ".NET",
-    },
-    {
-      id: "s2",
-      title: "React",
-    },
-    {
-      id: "s3",
-      title: "SQL",
-    },
-    {
-      id: "s4",
-      title: "Microsoft Azure",
-    },
-    {
-      id: "s5",
-      title: "Python",
-    },
-    {
-        id:"s6",
-        title:"TypeScript"
-    },
-  ];
+  // const DUMMY_SKILLS = [
+  //   {
+  //     id: "s1",
+  //     title: ".NET",
+  //   },
+  //   {
+  //     id: "s2",
+  //     title: "React",
+  //   },
+  //   {
+  //     id: "s3",
+  //     title: "SQL",
+  //   },
+  //   {
+  //     id: "s4",
+  //     title: "Microsoft Azure",
+  //   },
+  //   {
+  //     id: "s5",
+  //     title: "Python",
+  //   },
+  //   {
+  //     id: "s6",
+  //     title: "TypeScript",
+  //   },
+  // ];
 
+  const [skills, setSkills] = useState([]);
+  const { isLoading, error, sendRequest: fetchSkills } = useHttp();
+
+  useEffect(() => {
+    const transformSkills = (skillsObj) => {
+      const loadedSkills = [];
+
+      for (const skillKey in skillsObj) {
+        loadedSkills.push({
+          id: skillKey,
+          title: skillsObj[skillKey].title,
+        });
+      }
+      console.log(loadedSkills);
+      setSkills(loadedSkills);
+    };
+
+    fetchSkills({ url: "https://localhost:7277/api/Skills" }, transformSkills);
+  }, [fetchSkills]);
   return (
     <section id="skills">
       <Container className="px-5 py-10 mx-auto">
@@ -40,10 +62,17 @@ const Skills = () => {
             Skills
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            These are the skills and technologies that I have actively used in the past. I am always open to learning more to open my mind and expand my skillset.
+            These are the skills and technologies that I have actively used in
+            the past. I am always open to learning more to open my mind and
+            expand my skillset.
           </p>
         </div>
-        <SkillsList skills={DUMMY_SKILLS} />
+        <SkillsList
+          skills={skills}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchSkills}
+        />
       </Container>
     </section>
   );
--- src/components/Skills/SkillsList.js
+++ src/components/Skills/SkillsList.js
@@ -2,8 +2,10 @@ import { Col, Container, Row } from "react-bootstrap";
 import SkillItem from "./SkillItem/SkillItem";
 
 const SkillsList = (props) => {
-  return (
-    <Container>
+  let skillList = <h2>No skills have been found.</h2>
+
+  if(props.skills.length > 0){
+    skillList = (
       <Row xs={2} md={3} className="g-4">
         {props.skills.map((skill) => (
           <Col xs key={skill.id}>
@@ -11,6 +13,22 @@ const SkillsList = (props) => {
           </Col>
         ))}
       </Row>
+    );
+  }
+
+  let content = skillList;
+
+  if(props.error){
+    <h2>Something went wrong</h2>
+  }
+
+  if(props.loading){
+    content = 'Loading Skills...';
+  }
+
+  return (
+    <Container>
+      {content}
     </Container>
   );
 };
